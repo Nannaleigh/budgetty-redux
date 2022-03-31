b9c463ed312cc7381bff21d108b354d8359a4612
Nannaleigh <tatianna.martinez71@gmail.com>
2022-03-31 12:39:48 -0600

    Finished project

---
+++ src/redux/store.js
@@ -0,0 +1,14 @@
+/* eslint-disable no-unused-vars */
+import { createStore, combineReducers, applyMiddleware } from "redux";
+
+import promiseMiddleware from "redux-promise-middleware";
+
+import budgetReducer from "./budgetReducer";
+
+import userReducer from "./userReducer";
+const rootReducer = combineReducers({
+  budget: budgetReducer,
+  user: userReducer,
+});
+
+export default createStore(rootReducer, applyMiddleware(promiseMiddleware));
